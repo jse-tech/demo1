68be8ce8313aad0a87dd1dbf192c5d1d5ad63d9f
Conke Hu <conke.hu@maxwit.com>
2020-03-21 21:35:42 +0800

    test1
    Signed-off-by: Conke Hu <conke.hu@maxwit.com>

--- src/main/java/com/maxwit/App.java
+++ src/main/java/com/maxwit/App.java
@@ -5,6 +5,7 @@ package com.maxwit;
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        var s = "hello";
+        System.out.println(s);
     }
 }
